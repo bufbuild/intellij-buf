61dd907fc70383042e5edbc27c570bbe82a21a56
fedor <fedor.korotkov@gmail.com>
2022-03-18 10:01:05 -0400

    Check that index is populated

--- src/test/kotlin/com/github/bufbuild/intellij/completion/BufCompletionTest.kt
+++ src/test/kotlin/com/github/bufbuild/intellij/completion/BufCompletionTest.kt
@@ -1,6 +1,7 @@
 package com.github.bufbuild.intellij.completion
 
 import com.github.bufbuild.intellij.base.BufTestBase
+import com.github.bufbuild.intellij.index.BufModuleIndex
 import kotlin.test.assertContains
 
 class BufCompletionTest : BufTestBase() {
@@ -8,6 +9,7 @@ class BufCompletionTest : BufTestBase() {
 
     fun testExternalBufModule() {
         configureByFolder("external", "order.proto")
+        assertNotEmpty(BufModuleIndex.getAllProjectModules(project))
         val suggestions = myFixture.completeBasic().map { it.lookupString }
         assertContains(suggestions, "google/type/money.proto")
     }
--- src/test/kotlin/com/github/bufbuild/intellij/resolve/BufResolveTest.kt
+++ src/test/kotlin/com/github/bufbuild/intellij/resolve/BufResolveTest.kt
@@ -1,11 +1,13 @@
 package com.github.bufbuild.intellij.resolve
 
 import com.github.bufbuild.intellij.base.BufTestBase
+import com.github.bufbuild.intellij.index.BufModuleIndex
 
 class BufResolveTest : BufTestBase() {
     override fun getBasePath(): String = "resolve"
     fun testExternalBufModule() {
         configureByFolder("external", "order.proto")
+        assertNotEmpty(BufModuleIndex.getAllProjectModules(project))
         val reference = myFixture.getReferenceAtCaretPositionWithAssertion("order.proto")
         assertNotNull(reference.resolve())
     }
