f301a6fe68bd591847cca5fc8c819e441f2989d3
Alan Chiu <awlchiu@gmail.com>
2022-08-08 09:33:01 -0700

    Add IntelliJ version validation to github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,4 +111,8 @@ tasks {
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
         channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
     }
+
+    runPluginVerifier {
+        ideVersions.set(listOf("IIC-222.3739.24", "IIC-221.6008.8"))
+    }
 }
