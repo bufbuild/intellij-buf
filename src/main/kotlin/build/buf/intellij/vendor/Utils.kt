59e467c31d31820fb396968a2b4530511f74e723
caixuan <caixuan@baidu.com>
2023-12-27 20:56:20 +0800

    Fix tests

--- src/main/kotlin/build/buf/intellij/vendor/Utils.kt
+++ src/main/kotlin/build/buf/intellij/vendor/Utils.kt
@@ -52,9 +52,9 @@ private val kanroProtobufLanguage: Language? by lazy {
 }
 
 fun PsiFile.isProtobufFile(): Boolean {
-    return (kanroProtobufFile?.isInstance(this) ?: false) || (officialProtobufFile?.isInstance(this) ?: false)
+    return ((officialProtobufFile?.isInstance(this) ?: false || kanroProtobufFile?.isInstance(this) ?: false))
 }
 
 fun protobufLanguage(): Language? {
-    return kanroProtobufLanguage ?: officialProtobufLanguage
+    return officialProtobufLanguage ?: kanroProtobufLanguage
 }
