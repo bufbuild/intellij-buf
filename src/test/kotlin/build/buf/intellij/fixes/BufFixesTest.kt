e71fda70387a0be7dc04ede9292c6069a35c5d04
Philip K. Warren <pkwarren@users.noreply.github.com>
2024-01-23 11:56:58 -0600

    Prepare for the next plugin version (#173)
    Bump the version to 0.3.1. Update license year to include 2024. Upgrade
    gradle to 8.5. Update versions of IDEA that are tested to include the
    latest 2023.3.2 version.

--- src/test/kotlin/build/buf/intellij/fixes/BufFixesTest.kt
+++ src/test/kotlin/build/buf/intellij/fixes/BufFixesTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2022-2023 Buf Technologies, Inc.
+// Copyright 2022-2024 Buf Technologies, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ class BufFixesTest : BufTestBase() {
         myFixture.launchAction(intention)
         myFixture.checkResult(
             """
-            // Copyright 2022-2023 Buf Technologies, Inc.
+            // Copyright 2022-2024 Buf Technologies, Inc.
             //
             // Licensed under the Apache License, Version 2.0 (the "License");
             // you may not use this file except in compliance with the License.
