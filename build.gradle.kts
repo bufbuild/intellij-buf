d7650eb286450093d1f7a6ce7348a71576404579
Philip K. Warren <pkwarren@users.noreply.github.com>
2025-09-09 15:56:59 -0500

    Update IntelliJ platform plugin to latest (#359)
    Update the IntelliJ platform plugin to the latest 2.9.0. Add a new set
    of IDE ranges to test with plugin verification (existing `recommended()`
    is no longer working with the latest version). This will ensure we test
    with the full version range we support.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.models.ProductRelease
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
@@ -118,6 +120,12 @@ intellijPlatform {
     pluginVerification {
         ides {
             recommended()
+            select {
+                types = listOf(IntelliJPlatformType.IntellijIdea)
+                channels = listOf(ProductRelease.Channel.RELEASE)
+                sinceBuild = providers.gradleProperty("pluginSinceBuild")
+                untilBuild = providers.gradleProperty("pluginUntilBuild")
+            }
         }
         freeArgs = listOf(
             "-mute",
